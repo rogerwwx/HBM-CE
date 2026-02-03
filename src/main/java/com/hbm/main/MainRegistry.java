a87dc97016741617e73f3827d0b31712a8f0424b
mlbv <51232730+mlbv@users.noreply.github.com>
2026-02-03 14:49:46 +0800

    fix: fallout rain pinning threads in jvm

--- src/main/java/com/hbm/main/MainRegistry.java
+++ src/main/java/com/hbm/main/MainRegistry.java
@@ -25,6 +25,7 @@ import com.hbm.handler.*;
 import com.hbm.handler.imc.IMCHandler;
 import com.hbm.handler.pollution.PollutionHandler;
 import com.hbm.handler.radiation.RadiationSystemNT;
+import com.hbm.handler.threading.BombForkJoinPool;
 import com.hbm.handler.threading.PacketThreading;
 import com.hbm.hazard.HazardData;
 import com.hbm.hazard.HazardRegistry;
@@ -387,6 +388,7 @@ public class MainRegistry {
         RadiationSystemNT.onServerStopped();
         PhasedEventHandler.onServerStopped();
         PhasedStructureRegistry.onServerStopped();
+        BombForkJoinPool.onServerStopped();
     }
 
     @EventHandler
